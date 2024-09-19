5e34dd22add929bfa85c8690b2a6210064352343
DiveshR <divesh20july@gmail.com>
2024-09-19 20:23:47 +0530

    Roles and permission using spatie package

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::paginate(3);
+        $users = User::paginate(10);
         return view('users.index', compact('users'));
     }
 
